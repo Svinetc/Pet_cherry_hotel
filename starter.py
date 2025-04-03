7b2d37105034b908481c0cea21e716da7ea6e8ee
Svinets <109016419+Svinetc@users.noreply.github.com>
2025-04-03 21:35:13 +0300

    Add files via upload

---
+++ starter.py
@@ -0,0 +1,18 @@
+def check_ip(string:str):
+    res =[]
+    for i in string.split('.'):
+        res.append(i.lstrip('0'))
+
+
+    return all(map(lambda x : True if i.isdigit() and int(i)<=255 else False,res))
+
+
+
+
+if __name__=='__main__':
+    assert(check_ip('0.0.0.0'))==True
+    assert(check_ip('10.0.1.1'))==True
+    assert(check_ip('0.0.0.a'))==False
+    assert(check_ip('10.1.1.260'))==False
+    assert(check_ip('10.0023.0123.0000015'))==True
+    assert(check_ip('102.990.100.1'))==False
