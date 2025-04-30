07e78b750e49fa2fc36ce0d509d4c8d19e917ac6
Svinets <109016419+Svinetc@users.noreply.github.com>
2025-04-30 21:55:28 +0300

    Update lotery.py

--- lotery.py
+++ lotery.py
@@ -5,7 +5,7 @@ for i in range(n):
     row_total = 0
     count = 0
     for j in range(n):
-        if i == j and n- i -1 == n-j-1:
+        if i != j and n- i -1 == n-j-1:
             m[i][j]
         average = row_total//n
 
